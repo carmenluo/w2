becbaa2d1e2a3a70c80f48344ab89f0c36af9715
Carmen Luo <sdluojm@gmail.com>
2019-08-28 08:03:08 -0400

    update with promise

--- d2/profile_generator/survey.js
+++ d2/profile_generator/survey.js
@@ -1,46 +1,55 @@
-const profiles = [];
-let profile ={};
-const questions = ["What's your name? Nicknames are also acceptable\n", 
-"What's an activity you like doing?",
-"What do you listen to while doing that?",
-"Which meal is your favourite (eg: dinner, brunch, etc.)",
-"What's your favourite thing to eat for that meal?",
-"Which sport is your absolute favourite?",
-"What is your superpower? In a few words, tell us what you are amazing at!"
+let profile = {
+  name:'',
+  act: '',
+  music: '',
+  meal:'',
+  thing: '',
+  sprot:'',
+  superpower: ''
+};
+const questions = ["What's your name? Nicknames are also acceptable\n",
+  "What's an activity you like doing?\n",
+  "What do you listen to while doing that?\n",
+  "Which meal is your favourite (eg: dinner, brunch, etc.)\n",
+  "What's your favourite thing to eat for that meal?\n",
+  "Which sport is your absolute favourite?\n",
+  "What is your superpower? In a few words, tell us what you are amazing at!\n"
 ];
+let answers = [];
 const readline = require('readline');
 const r1 = readline.createInterface({
-  input : process.stdin,
+  input: process.stdin,
   output: process.stdout
 });
-r1.question("What's your name? Nicknames are also acceptable\n",(answer) =>{
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-  profile.name = answer;
-//  r1.close();
-})
 
-// var read = require('readline'),
-//     input = read.createInterface(process.stdin, process.stdout),
-//     questions = ['test1: ', 'test2: '],
-//     counter = 0;
+function askQuestion() {
+  return new Promise((resolve, reject) => {
+    // resolve to active 'then' chaining (empty array for answers)
+    let chainQ = Promise.resolve([]);
+    questions.forEach(question => {
+      chainQ = chainQ.then(answers => new Promise((resolveQ, rejectQ) => {
+        r1.question(`${question}`, (answer) => {
+          console.log(`Thank you for you feedback: ${answer}\n`);
+          answers.push(answer);
+          resolveQ(answers);
+        })
+      }))
+    });
+    //Have to call then to return answers, otherwise it will return at the beginning without waiting for all questions
+    chainQ.then(answers => {
+      r1.close();
+      resolve(answers);
+    })
+  })
+}
 
-// input.setPrompt(questions[0]);
-// input.prompt();
-
-// input.on('line', function (a) {
-//     console.log('answer: ', a);
-//     counter++;
-//     if (counter < questions.length) {
-//         input.setPrompt(questions[counter]);
-//         input.prompt();
-//     } else {
-//         process.exit(0);
-//     }
-// });
-// What's your name? Nicknames are also acceptable :)
-// What's an activity you like doing?
-// What do you listen to while doing that?
-// Which meal is your favourite (eg: dinner, brunch, etc.)
-// What's your favourite thing to eat for that meal?
-// Which sport is your absolute favourite?
-// What is your superpower? In a few words, tell us what you are amazing at!
+let result = askQuestion();
+result
+.then(data =>{
+  let i = 0;
+  for (let item in profile){
+    profile[item] = data[i];
+    i++;
+  }
+  console.log(profile);
+});
